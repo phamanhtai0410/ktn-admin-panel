853a9f7cc55d683573c59284348827f4fb4753ed
PTChinh <9a2.phamtranchinh@gmail.com>
2022-10-19 14:51:31 +0700

    Update create multi promotion code

--- src/views/models/promotion.py
+++ src/views/models/promotion.py
@@ -1,4 +1,11 @@
+import random
+import string
+
+from flask import request, redirect
+from flask_admin import expose
 from flask_admin.model.template import DeleteRowAction, EditRowAction
+
+from src.models.promotion import Promotion
 from src.views.base import MyBaseModelView, RowActionListMixin
 
 
@@ -10,7 +17,22 @@ class PromotionView(RowActionListMixin, MyBaseModelView):
 
     column_searchable_list = ['code']
 
+    list_template = 'custom/menu_bar.html'
+
     column_default_sort = ('created_time', True)
+
+    @expose('/create_multi', methods=['POST'])
+    def create_multi(self):
+        quantity = int(request.form['quantity'])
+        discount = int(request.form['discount'])
+        length = 8
+
+        for i in range(quantity):
+            letters = string.ascii_uppercase + string.digits
+            code = ''.join(random.choice(letters) for i in range(length))
+            Promotion(code=code, discount=discount, status=True).save()
+
+        return redirect('/promotion/')
 
     def _can_edit(self, model):
         # Put your logic here to allow edit per model
