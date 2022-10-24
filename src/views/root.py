ae77471f63745d5da1e174a3597baed1c49bd89d
thanh-hdt <thanh.hoang@atomsolution.vn>
2022-10-24 16:18:52 +0700

    update nft_type

--- src/views/root.py
+++ src/views/root.py
@@ -7,7 +7,7 @@
 from flask_login import login_required
 from jinja2 import TemplateNotFound
 
-from flask import Blueprint, request, render_template, redirect, url_for
+from flask import Blueprint, request, render_template, redirect, url_for, jsonify
 
 root_blueprint = Blueprint(
     'root_blueprint',
@@ -35,3 +35,17 @@ def get_segment(request):
 @login_required
 def root_view():
     return redirect(url_for('admin.index'))
+
+# @root_blueprint.route('/get_nft_detail', methods= ['POST'])
+# @login_required
+# def index():
+#     nfts= {
+#         'nft_id': 1,
+#         'name': 'thanh',
+#         'rarity': 2,
+#         'is_active': True
+#     }
+#     print(nfts)
+#     return jsonify({'htmlresponse': render_template('custom/nft_type.html',nfts=nfts)})
+    # return nfts
+    # return redirect(url_for('admin.index'))
