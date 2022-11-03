c8a7f7372bdcd4af5f446db504a1c2ddb95563a9
thanh-hdt <thanh.hoang@atomsolution.vn>
2022-11-03 10:53:01 +0700

    delete status promotion code

--- src/views/models/promotion.py
+++ src/views/models/promotion.py
@@ -10,7 +10,7 @@ from src.views.base import MyBaseModelView, RowActionListMixin
 
 
 class PromotionView(RowActionListMixin, MyBaseModelView):
-    column_list = ['code', 'discount', 'used', 'total', 'status', 'created_time', 'updated_time', 'updated_by']
+    column_list = ['code', 'discount', 'used', 'total', 'created_time', 'updated_time', 'updated_by']
     can_edit = True
     can_create = True
     can_delete = True
@@ -35,14 +35,15 @@ class PromotionView(RowActionListMixin, MyBaseModelView):
         return redirect('/promotion/')
 
     def _can_edit(self, model):
+        
         # Put your logic here to allow edit per model
         # return True to allow edit
-        return model.status
+        return not (model.used >= model.total)
 
     def _can_delete(self, model):
         # Put your logic here to allow delete per model
         # return True to allow delete
-        return model.status
+        return not (model.used >= model.total)
 
     def allow_row_action(self, action, model):
 
